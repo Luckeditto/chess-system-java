56559468a8a8ddcb2d5eb9257726809fb6abfbac
Luckeditto <lucasclub69@email.com>
2023-12-20 11:22:42 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,7 +40,15 @@ public class Program {
 					captured.add(capturedPiece);
 				}
 
-			}catch (ChessException | InputMismatchException e){
+				if (chessMatch.getPromoted() != null){
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+
+
+			}
+			catch (ChessException | InputMismatchException e){
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
